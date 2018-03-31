83a0c86c8ba24ade11e8d0867acbb71c25676ea5
Haozhuo <vichuanghaozhuo@outlook.com>
2018-03-31 11:34:05 -0700

    change index page response to json

--- driver_alter_backend/user/views.py
+++ driver_alter_backend/user/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import DataPoint, Trip, UserData
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Data received</h1>")
+    return JsonResponse({"data":"received"})
     
